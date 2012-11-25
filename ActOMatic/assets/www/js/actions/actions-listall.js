c4dd03311adf92e8ac73679fc21cf48321b18373
Eiji Adachi <eijiadachi@gmail.com>
2012-11-25 19:36:40 -0200

    listing all in a table

--- ActOMatic/assets/www/js/actions/actions-listall.js
+++ ActOMatic/assets/www/js/actions/actions-listall.js
@@ -12,10 +12,34 @@ var successCallback = function( arg )
 	
 	var size = arg.length;
 	
-	var str = "";
+	var str = "<table class='table table-bordered table-striped'>";
+	str += "<thead><tr>";
+	str += "<th>#</th>";
+	str += "<th>Name</th>";
+	str += "<th>Description</th>";
+	str += "<th>Type</th>";
+	str += "<th>Setting</th>";
+	str += "</tr></thead>";
+	
+	str += "<tbody>";
+	
 	for( var i = 0; i < size; i++ ){
-		str += arg[i] + "\n";
+		var obj = jQuery.parseJSON( arg[i] );
+		
+		console.log(obj);
+		
+		str += "<tr>";
+		
+		str += "<td>" + (i+1) + "</td>";
+		str += "<td>" + obj.name + "</td>";
+		str += "<td>" + obj.description + "</td>";
+		str += "<td>" + obj.type + "</td>";
+		str += "<td>" + obj.setting + "</td>";
+		
+		str += "</tr>";
 	}
+	
+	str += "</tbody></table>";
 	
 	$("#listAllDiv").html(str);
 	console.log(str);
@@ -24,7 +48,7 @@ var successCallback = function( arg )
 var errorCallback = function( arg )
 {
 	console.log( "Calling the method " + methodName + " returned unsuccessfully with the arguments: " + arg );
-	alert( "It was not possible to create the action.\n\n" + arg );
+	alert( "It was not possible to list all the actions.\n\n" + arg );
 }
 
 ACTIONS_LISTALL.init = function()
